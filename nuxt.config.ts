40816864ba137470415bd5d3e83d671b127e2dcb
RosDeeper <hoanggg237@gmail.com>
2024-09-09 22:57:52 +0700

    project setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  pages: true,
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+    // '@nuxtjs/supabase',
+    '@nuxt/icon',
+    'nuxt-lodash',
+  ],
+  app: {
+    head: {
+      title: 'AliExpress Clone',
+      meta: [
+        {name: 'viewport', content: 'width=device-width, initial-scale=1'}
+      ]
+    }
+  }
 })
