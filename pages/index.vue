99d3777b674f934f3172b0500c02a50b0bb3d8bf
RosDeeper <hoanggg237@gmail.com>
2024-09-13 11:13:05 +0700

    product page

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
 	<MainLayout>
 		<div id="IndexPage" class="mt-4 max-w-[1440px] mx-auto px-2">
 			<div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
-				<div></div>
+				<div v-if="products" v-for="product in products" :key="product">
+					<ProductComponent :product="product"/>
+				</div>
 			</div>
 		</div>
 	</MainLayout>
@@ -10,5 +12,15 @@
 
 <script setup>
 	import MainLayout from '~/layouts/MainLayout.vue'
+
+	const products = [
+		{ id: 1, title: 'Title 1', discription: 'This is a description', url: 'https://picsum.photos/id/7/300/300', price: 9999 },
+		{ id: 2, title: 'Title 2', discription: 'This is a description', url: 'https://picsum.photos/id/77/300/300', price: 9999 },
+		{ id: 3, title: 'Title 3', discription: 'This is a description', url: 'https://picsum.photos/id/78/300/300', price: 9999 },
+		{ id: 4, title: 'Title 4', discription: 'This is a description', url: 'https://picsum.photos/id/79/300/300', price: 9999 },
+		{ id: 5, title: 'Title 5', discription: 'This is a description', url: 'https://picsum.photos/id/72/300/300', price: 9999 },
+		{ id: 6, title: 'Title 6', discription: 'This is a description', url: 'https://picsum.photos/id/74/300/300', price: 9999 },
+		{ id: 7, title: 'Title 7', discription: 'This is a description', url: 'https://picsum.photos/id/76/300/300', price: 9999 },
+	]
 </script>
 
---
+++ pages/item/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <MainLayout>
+        <div id="ItemPage" class="mt-4 max-w-[1440px] mx-auto px-2">
+            <div class="md:flex gap-4 justify-between mx-auto w-full">
+                <div class="md:w-[40%]">
+                    <img src="https://picsum.photos/id/7/300/300" class="rounded-lg object-fit">
+                </div>
+            </div>
+        </div>
+    </MainLayout>
+</template>
+
+<script setup>
+    import MainLayout from '~/layouts/MainLayout.vue'
+</script>
