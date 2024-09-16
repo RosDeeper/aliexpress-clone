15b8a95dbae0ca3c555fe5832b2ccf5dd214a4ea
RosDeeper <hoanggg237@gmail.com>
2024-09-16 08:28:05 +0700

    checkout page #2

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -52,6 +52,56 @@
                             Add New Address
                         </NuxtLink>
                     </div>
+
+                    <div id="Items" class="bg-white rounded-lg p-4 mt-4">
+                        <div v-for="product in products">
+                            <CheckoutItem :product="product"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="md:hidden block my-4"></div>
+
+                <div class="md:w-[35%]">
+                    <div id="PlaceOrder" class="bg-white rounded-lg p-4">
+                        <div class="text-2xl font-extrabold mb-2">Summary</div>
+
+                        <div class="flex items-center justify-between my-4">
+                            <div class="">Total Shipping</div>
+                            <div class="">Free</div>
+                        </div>
+
+                        <div class="border-t"></div>
+
+                        <div class="flex items-center justify-between my-4">
+                            <div class="font-semibold">Total</div>
+                            <div class="text-2xl font-semibold">
+                                $ <span class="font-extrabold">{{ total / 100 }}</span>
+                            </div>
+                        </div>
+
+                        <form @submit.prevent="pay()">
+                            <div class="border border-gray-500 p-2 rounded-sm" id="card-element"></div>
+                            <p id="card-error" role="alert" class="text-red-700 text-center font-semibold"></p>
+
+                            <button
+                                :disabled="isProcessing"
+                                type="submit"
+                                class="mt-4 bg-gradient-to-r from-[#FE630C] to-[#FF3200] w-full text-white text-[21px] font-semibold p-1.5 rounded-full"
+                                :class="isProcessing ? 'opacity-70' : 'opacity-100'"
+                            >
+                                <Icon v-if="isProcessing" name="eos-icons:loading" />
+                                <div v-else>Place order</div>
+                            </button>
+                        </form>
+                    </div>
+
+                    <div class="bg-white rounded-lg p-4 mt-4">
+                        <div class="text-lg font-semibold mb-2 mt-2">AliExpress</div>
+                        <p class="my-2">
+                            AliExpress keeps your information and payment safe
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
